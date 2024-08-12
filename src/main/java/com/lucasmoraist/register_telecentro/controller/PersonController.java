362d6d16ac113285bf62697957df799ca3c773a1
lucasmoraist <luksmnt1101@gmail.com>
2024-08-12 10:59:19 -0300

    feat: add request to get person by rg

--- src/main/java/com/lucasmoraist/register_telecentro/controller/PersonController.java
+++ src/main/java/com/lucasmoraist/register_telecentro/controller/PersonController.java
@@ -1,14 +1,10 @@
 package com.lucasmoraist.register_telecentro.controller;
 
-import com.lucasmoraist.register_telecentro.model.Course;
 import com.lucasmoraist.register_telecentro.model.Person;
 import com.lucasmoraist.register_telecentro.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -23,6 +19,11 @@ public class PersonController {
     public ResponseEntity<Void> save(@RequestBody Person person) throws IOException {
         this.service.save(person);
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Person> getByName(@RequestParam String rg) throws IOException {
+        return ResponseEntity.ok().body(this.service.getPersonByRg(rg));
     }
 
 }

66270aa5e1e51f5890b41baa50f8bdb0d4ae20cd
lucasmoraist <luksmnt1101@gmail.com>
2024-08-12 14:08:32 -0300

    chore: add documentation with JavaDoc

--- src/main/java/com/lucasmoraist/register_telecentro/repository/PersonRepository.java
+++ src/main/java/com/lucasmoraist/register_telecentro/repository/PersonRepository.java
@@ -1,10 +1,15 @@
 package com.lucasmoraist.register_telecentro.repository;
 
-import com.lucasmoraist.register_telecentro.model.Course;
 import com.lucasmoraist.register_telecentro.model.Person;
 
 import java.io.IOException;
 
+/**
+ * This interface defines the methods that must be implemented by a class that will be responsible for saving
+ * and retrieving
+ *
+ * @author lucasmoraist
+ */
 public interface PersonRepository {
     void save(Person person) throws IOException;
     Person getPersonByRg(String rg) throws IOException;

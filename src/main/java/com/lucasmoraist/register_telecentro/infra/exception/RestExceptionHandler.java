c074d0de61ac81b43c76539b6354c51b71e7d208
lucasmoraist <luksmnt1101@gmail.com>
2024-08-12 14:10:57 -0300

    chore: add documentation with JavaDoc

--- src/main/java/com/lucasmoraist/register_telecentro/infra/exception/RestExceptionHandler.java
+++ src/main/java/com/lucasmoraist/register_telecentro/infra/exception/RestExceptionHandler.java
@@ -2,8 +2,10 @@ package com.lucasmoraist.register_telecentro.infra.exception;
 
 import com.lucasmoraist.register_telecentro.exceptions.ExceptionDTO;
 import com.lucasmoraist.register_telecentro.exceptions.ResourceNotFound;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailAuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -11,27 +13,65 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+@Slf4j
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
+    /**
+     * Handles ResourceNotFound exceptions.
+     *
+     * @param ex the ResourceNotFound exception
+     * @return a ResponseEntity containing the exception details
+     */
     @ExceptionHandler(ResourceNotFound.class)
     protected ResponseEntity<ExceptionDTO> handleResourceNotFound(ResourceNotFound ex) {
         return ResponseEntity.badRequest().body(
                 new ExceptionDTO(ex.getMessage(), HttpStatus.NOT_FOUND));
     }
 
+    /**
+     * Handles IOException exceptions.
+     *
+     * @param ex the IOException
+     * @return a ResponseEntity containing the exception details
+     */
     @ExceptionHandler(IOException.class)
     protected ResponseEntity<ExceptionDTO> handleIOException(IOException ex) {
         return ResponseEntity.badRequest().body(
                 new ExceptionDTO(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR));
     }
 
+    /**
+     * Handles FileNotFoundException exceptions.
+     *
+     * @param ex the FileNotFoundException
+     * @return a ResponseEntity containing the exception details
+     */
     @ExceptionHandler(FileNotFoundException.class)
     protected ResponseEntity<ExceptionDTO> handleFileNotFoundException(FileNotFoundException ex) {
         return ResponseEntity.badRequest().body(
                 new ExceptionDTO(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR));
     }
 
+    /**
+     * Handles MailAuthenticationException exceptions.
+     *
+     * @param e the MailAuthenticationException
+     * @return a ResponseEntity containing the exception details
+     */
+    @ExceptionHandler(MailAuthenticationException.class)
+    protected ResponseEntity<ExceptionDTO> sendMailException(MailAuthenticationException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                new ExceptionDTO(e.getMessage(), HttpStatus.BAD_REQUEST)
+        );
+    }
+
+    /**
+     * Handles generic exceptions.
+     *
+     * @param ex the generic exception
+     * @return a ResponseEntity containing the exception details
+     */
     @ExceptionHandler(Exception.class)
     protected ResponseEntity<ExceptionDTO> handleException(Exception ex) {
         return ResponseEntity.badRequest().body(

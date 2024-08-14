6a7a9f3e74e9253b8a6aa5a173017cac992c6b1f
lucasmoraist <luksmnt1101@gmail.com>
2024-08-14 11:43:11 -0300

    chore: configuration cors

--- src/main/java/com/lucasmoraist/register_telecentro/infra/cors/CorsConfig.java
+++ src/main/java/com/lucasmoraist/register_telecentro/infra/cors/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET", "POST", "DELETE", "PUT")
                         .allowedHeaders("*")
                         .allowCredentials(true);

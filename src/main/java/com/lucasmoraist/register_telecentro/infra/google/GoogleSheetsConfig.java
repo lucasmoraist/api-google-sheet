31f99cd1038693fe73b1f909f6ca40475f40216d
lucasmoraist <luksmnt1101@gmail.com>
2024-08-16 09:09:54 -0300

    chore: Update Google Sheets configuration with latest client libraries

--- src/main/java/com/lucasmoraist/register_telecentro/infra/google/GoogleSheetsConfig.java
+++ src/main/java/com/lucasmoraist/register_telecentro/infra/google/GoogleSheetsConfig.java
@@ -1,16 +1,9 @@
 package com.lucasmoraist.register_telecentro.infra.google;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpRequestInitializer;
-import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
 import com.google.auth.http.HttpCredentialsAdapter;

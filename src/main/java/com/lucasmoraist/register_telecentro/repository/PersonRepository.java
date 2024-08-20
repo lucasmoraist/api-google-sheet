d4004a55cfb8a14676f106a2fae2491404e5f3a4
lucasmoraist <luksmnt1101@gmail.com>
2024-08-20 15:24:15 -0300

    chore: Update PersonRepository interface to include new methods for saving and retrieving person data

--- src/main/java/com/lucasmoraist/register_telecentro/repository/PersonRepository.java
+++ src/main/java/com/lucasmoraist/register_telecentro/repository/PersonRepository.java
@@ -3,6 +3,7 @@ package com.lucasmoraist.register_telecentro.repository;
 import com.lucasmoraist.register_telecentro.model.Person;
 
 import java.io.IOException;
+import java.util.Optional;
 
 /**
  * This interface defines the methods that must be implemented by a class that will be responsible for saving
@@ -11,6 +12,8 @@ import java.io.IOException;
  * @author lucasmoraist
  */
 public interface PersonRepository {
-    void save(Person person) throws IOException;
-    Person getPersonByRg(String rg) throws IOException;
+    int getNextAvailableRow() throws IOException;
+    void savePersonData(Person person, String range) throws IOException;
+    Optional<Person> findPersonByRg(String rg) throws IOException;
+    boolean isRgAndCourseDateAlreadyRegistered(String rg, String courseDate) throws IOException;
 }

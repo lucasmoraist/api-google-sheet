2ca8184cade37b36410fb5dba9fc17c92390f42c
lucasmoraist <luksmnt1101@gmail.com>
2024-08-26 15:40:38 -0300

    feat: Add CourseRepository and CourseImpl for course data management

---
+++ src/main/java/com/lucasmoraist/register_telecentro/repository/CourseRepository.java
@@ -0,0 +1,12 @@
+package com.lucasmoraist.register_telecentro.repository;
+
+import com.lucasmoraist.register_telecentro.model.Course;
+import com.lucasmoraist.register_telecentro.model.dto.UpdateCourse;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface CourseRepository {
+    List<Course> listAll() throws IOException;
+    void updateCourseDateTime(UpdateCourse dto) throws IOException;
+}

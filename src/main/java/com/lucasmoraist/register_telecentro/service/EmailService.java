711c5d76733f66656a99d28d1abb729be06218fe
lucasmoraist <luksmnt1101@gmail.com>
2024-08-30 22:44:25 -0300

    refactor: Improve email sending functionality in EmailService

--- src/main/java/com/lucasmoraist/register_telecentro/service/EmailService.java
+++ src/main/java/com/lucasmoraist/register_telecentro/service/EmailService.java
@@ -1,8 +1,10 @@
 package com.lucasmoraist.register_telecentro.service;
 
+import com.lucasmoraist.register_telecentro.exceptions.SendMailException;
 import com.lucasmoraist.register_telecentro.model.Person;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -19,6 +21,7 @@ import java.time.format.DateTimeFormatter;
  * @author lucasmoraist
  */
 @Service
+@Slf4j
 public class EmailService {
 
     @Autowired
@@ -29,6 +32,7 @@ public class EmailService {
 
     /**
      * Sends an email to the person who registered for the course.
+     *
      * @param person the person who registered for the course
      * @throws MessagingException if an error occurs while sending the email
      */
@@ -40,29 +44,29 @@ public class EmailService {
         helper.setSubject("Inscrição no curso de %s feita com sucesso!".formatted(person.getCourse().getNameCourse()));
 
         String htmlContent = """
-            <html>
-            <body>
-                <h2>Curso de <strong>%s</strong></h2>
-                <p>Data e hora: <strong>%s</strong></p>
-            
-                <p>Abraços,</p>
-                <p>Agente de Inclusão Digital<br>(011) 5667-6272</p>
-            
-                <i>Data de envio: %s</>
-            </body>
-            </html>
-            """.formatted(
-                    person.getCourse().getNameCourse(),
-                    person.getCourse().getDateAndTime(),
-                    now.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"))
-                );
+                <html>
+                <body>
+                    <h2>Curso de <strong>%s</strong></h2>
+                    <p>Data e hora: <strong>%s</strong></p>
+                            
+                    <p>Abraços,</p>
+                    <p>Agente de Inclusão Digital<br>(011) 5667-6272</p>
+                            
+                    <i>Data de envio: %s</>
+                </body>
+                </html>
+                """.formatted(
+                person.getCourse().getNameCourse(),
+                person.getCourse().getDateAndTime(),
+                now.format(DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"))
+        );
 
 
         helper.setText(htmlContent, true);
 
         helper.setFrom(from);
-
         mailSender.send(message);
+
     }
 
 }
